b265b956b7b4b1599890f4a2a34448d42b96d45b
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-27 19:02:06 +0200

    refactor: replace contact dropdown with social media links
    - Replaced the contact dropdown with social media links using their respective logos.

--- src/constants/social-networks.ts
+++
@@ -1,6 +0,0 @@
-export const SOCIAL_NETWORKS = {
-  instagram: 'https://www.instagram.com/raulf1foreveryt_oficial',
-  twitter: 'https://twitter.com/CatalinasRaul',
-  github: 'https://github.com/RaulCatalinas',
-  facebook: 'https://www.facebook.com/raul.catalinasesteban'
-} as const
