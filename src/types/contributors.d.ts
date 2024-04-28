bb6432044454a852ed924aaa2a377e9bf145e975
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-28 14:03:40 +0200

    feat: feat: add contributors section
    - Implemented a section to display contributors.

---
+++ src/types/contributors.d.ts
@@ -0,0 +1,11 @@
+export interface Contributors {
+  name: string
+  imageURL: string
+  socialNetworks: SocialNetwork[]
+}
+
+export interface SocialNetwork {
+  name: string
+  link: string
+  imageURL: string
+}
